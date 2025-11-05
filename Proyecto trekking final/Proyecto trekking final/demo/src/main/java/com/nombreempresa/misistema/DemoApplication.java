3d7d714431b66e5854456289dbea74c7eec744f3
ariana progano <elmallian@gmail.com>
2025-11-04 21:31:46 -0300

    comentarios

--- Proyecto trekking final/Proyecto trekking final/demo/src/main/java/com/nombreempresa/misistema/DemoApplication.java
+++ Proyecto trekking final/Proyecto trekking final/demo/src/main/java/com/nombreempresa/misistema/DemoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
-// Quitamos el parametro 'enabled' que da error.
-// El Repositorio será ignorado de todas formas ya que el controlador usa Neo4jClient.
 @EnableNeo4jRepositories(basePackages = "com.nombreempresa.misistema.repo")
 public class DemoApplication {
     public static void main(String[] args) {
